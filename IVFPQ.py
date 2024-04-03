bec1734801884b89ab292addfaeaf35188d0b2d3
HP <vanhao.truong@gmail.com>
2024-04-03 22:08:06 +0700

    1. Fixed issue index mismatch betweem features_labels_filenames. 2. Added IVFPQ method

--- IVFPQ.py
+++ IVFPQ.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.applications.efficientnet import EfficientNetB0, preprocess_input
 from keras.preprocessing import image
-import glob, tqdm, faiss, os
+import glob, tqdm, faiss
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -10,27 +10,36 @@ import matplotlib.image as mpimg
 model = EfficientNetB0(weights='imagenet', include_top=False, pooling='avg')
 target_size = (150, 150)
 dim = 1280  # EffientNetB0 output has 1280 features when input image size is (150, 150)
-nbit= 64
+nbit= 8
 
 
-####### Train faiss.IndexLSH
-index = faiss.IndexLSH(dim, nbit)
+####### Train faiss.IndexIVFPQ
+quantizer = faiss.IndexFlatL2(dim)
+index = faiss.IndexIVFPQ(quantizer, dim, 256, 8, nbit)
+
 train_paths = glob.glob('./features/total_train/**/*.npz', recursive=True)
 train_paths = sorted(train_paths)
 
 labels = []
 file_names = []
+all_features = []
 for train_path in tqdm.tqdm(train_paths):
     traindata = np.load(train_path)
 
-    index.add(traindata['features'])
+    all_features.append(traindata['features'])
     labels.extend(traindata['labels'])
     file_names.extend(traindata['file_names'])
 
-faiss.write_index(index, 'IndexLSH.index')
+all_features = np.vstack(all_features)
+index.train(all_features)
+
+# Add all features to the index
+index.add(all_features)
+
+faiss.write_index(index, 'IndexIVFPQ.index')
 
 ######## Inference
-index = faiss.read_index('IndexLSH.index')
+index = faiss.read_index('IndexIVFPQ.index')
 print(f"Number of images added to the index: {index.ntotal}")
 
 predict_files = glob.glob('./Intel_Classification_Dataset/seg_pred/**/*.jpg', recursive=True)
@@ -46,7 +55,6 @@ features = model.predict(x)
 D, I = index.search(features, k= nneighbors)
 print(D)
 print(I)
-
 # Display the query image
 plt.figure(figsize=(60, 20))
 plt.subplot(1, nneighbors + 1, 1)
@@ -59,6 +67,7 @@ for i, ind in enumerate(I[0]):
     plt.title(f'Rank {i+1}, Label: {labels[ind]}')
 
 plt.show()
+
 
 
 
--- extract_feature.py
+++ extract_feature.py
@@ -7,24 +7,32 @@ import numpy as np
 model = EfficientNetB0(weights='imagenet', include_top=False, pooling='avg')
 target_size = (150, 150)
 
+files = glob.glob('./features/total_train/**/*.*', recursive=True)
+for f in files:
+    os.remove(f)
+
 # train_files
 train_files = glob.glob('./Intel_Classification_Dataset/seg_train/**/*.jpg', recursive=True)
 test_files = glob.glob('./Intel_Classification_Dataset/seg_test/**/*.jpg', recursive=True)
 
 total_train_files = train_files + test_files
+total_train_files = sorted(total_train_files)
 
-batch_size = 64
-labels = []  # List to store the labels
-file_name = []
+batch_size = 640
 
 for i in range(0, len(total_train_files), batch_size):
     batch_files = total_train_files[i:i+batch_size]
     batch_images = []
+    
+    labels = []  # List to store the labels
+    file_names = []
+
     for file in batch_files:
         # Extract the label from the file path and add it to the labels list
         label = os.path.basename(os.path.dirname(file))
+
         labels.append(label)
-        file_name.append(file)
+        file_names.append(file)
 
         img = image.load_img(file, target_size=target_size)
         x = image.img_to_array(img)
@@ -35,9 +43,6 @@ for i in range(0, len(total_train_files), batch_size):
     batch_images = np.vstack(batch_images)
 
     features = model.predict(batch_images)
-    np.save(f'./features/total_train/batch_number_{i//batch_size}.npy', features)
-
-
-# Save the labels and file_name to a file
-np.save('./features/labels.npy', labels)
-np.save('./features/file_name.npy', file_name)
+    np.savez(f'./features/total_train/batch_number_{i//batch_size}.npz', 
+             features= features, labels= labels, file_names= file_names)
+    print(f'Batch number {i//batch_size} saved')
